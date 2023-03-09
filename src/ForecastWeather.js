2a455bcbfa666085af2e7c9288c3e1b001e28771
tohhuishan <tohhuishan12@gmail.com>
2023-03-09 10:03:16 +0800

    Set up the structure of the forecast section

--- src/ForecastWeather.js
+++ src/ForecastWeather.js
@@ -2,5 +2,85 @@ import React from "react";
 import "./ForecastWeather.css";
 
 export default function ForecastWeather() {
-  return <div className="ForecastWeather row"></div>;
+  return (
+    <div className="ForecastWeather row">
+      <div className="col-4">
+        <div className="day">Thu</div>
+        <div>
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/mist-night.png"
+            alt=""
+            width={8}
+            className="forecast-weather-icon"
+          />
+        </div>
+        <span>19° 25°</span>
+      </div>
+
+      <div className="col-4">
+        <div className="day">Thu</div>
+        <div>
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/mist-night.png"
+            alt=""
+            width={8}
+            className="forecast-weather-icon"
+          />
+        </div>
+        <span>19° 25°</span>
+      </div>
+
+      <div className="col-4">
+        <div className="day">Thu</div>
+        <div>
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/mist-night.png"
+            alt=""
+            width={8}
+            className="forecast-weather-icon"
+          />
+        </div>
+        <span>19° 25°</span>
+      </div>
+
+      <div className="col-4">
+        <div className="day">Thu</div>
+        <div>
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/mist-night.png"
+            alt=""
+            width={8}
+            className="forecast-weather-icon"
+          />
+        </div>
+        <span>19° 25°</span>
+      </div>
+
+      <div className="col-4">
+        <div className="day">Thu</div>
+        <div>
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/mist-night.png"
+            alt=""
+            width={8}
+            className="forecast-weather-icon"
+          />
+        </div>
+        <span>19° 25°</span>
+      </div>
+
+      <div className="col-4">
+        <div className="day">Thu</div>
+        <div>
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/mist-night.png"
+            alt=""
+            width={8}
+            className="forecast-weather-icon"
+          />
+        </div>
+        <span>19° 25°</span>
+      </div>
+    </div>
+  );
 }
